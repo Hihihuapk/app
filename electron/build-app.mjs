97fecb312d2f580778a47cbc61b00793947f2192
Hihihuapk <hoadavidnguyen8@gmail.com>
2025-03-27 09:21:40 +0700

    Create build-app.mjs

---
+++ electron/build-app.mjs
@@ -0,0 +1,35 @@
+import path from 'node:path';
+import fs from 'node:fs/promises';
+import { purge, run } from '../../tools.mjs';
+
+const dirBuild = path.resolve('build');
+const pkgFile = 'package.json';
+const pkgConfig = JSON.parse(await fs.readFile(pkgFile));
+const targetFile = path.resolve(dirBuild, pkgFile);
+let targetConfig = {};
+try {
+    targetConfig = JSON.parse(await fs.readFile(targetFile));
+} catch { /* IGNORE */ }
+
+await purge(dirBuild);
+
+const manifest = {
+    name: pkgConfig.name,
+    //type: pkgConfig.type, // 'commonjs',
+    main: pkgConfig.main,
+    //'node-main': pkgConfig.main,
+    url: pkgConfig.url,
+    'node-remote': [
+        'http://localhost/*',
+        'https://*.pages.dev/*',
+        'https://app.hihihuapk.ovh/*',
+        'https://app.hihihuapk.download/*',
+        `${new URL(pkgConfig.url).origin}/*`,
+    ],
+    'chromium-args': null,
+    'user-agent': targetConfig['user-agent'] ?? null,
+    dependencies: pkgConfig.dependencies
+};
+
+await fs.writeFile(targetFile, JSON.stringify(manifest, null, 4));
+await run('npm install --omit=dev', dirBuild);
